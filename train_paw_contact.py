b09d000583b4d41c43c021eed999d8b307b17f05
Edward Li <edwardyaoli@gmail.com>
2019-06-21 14:06:49 -0400

    first train:

---
+++ train_paw_contact.py
@@ -0,0 +1,24 @@
+from fastai.imports import *
+from fastai.transforms import *
+from fastai.conv_learner import *
+from fastai.model import *
+from fastai.dataset import *
+from fastai.sgdr import *
+from fastai.plots import *
+
+import os
+import sys
+import argparse
+
+arch = resnext50
+
+def get_data(sz, PATH, bs=64):
+    tfms = tfms_from_model(arch, sz, aug_tfms=transforms_basic, max_zoom=1.1)
+    return ImageClassifierData.from_paths(PATH, bs=bs, tfms=tfms)
+
+if __name__ == '__main__':
+    if not os.path.exists(os.path.join('fastai', 'weights')):
+        print("[ERROR] Manual prerun step required")
+        print("Download this file: http://files.fast.ai/models/weights.tgz")
+        print("And extract it in the 'fastai' directory")
+        sys.exit()
